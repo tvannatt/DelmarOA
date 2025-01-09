a4f88621ee1866f1c914ca223de81bfeb4e417cc
Trevor Vannatta <129558302+tvannatt@users.noreply.github.com>
2025-01-09 13:03:45 -0500

    readme update

--- .venv/LoyalCustomer.py
+++ .venv/LoyalCustomer.py
@@ -1,22 +1,29 @@
 import pandas as pd
 import duckdb
-from pathlib import Path
+import glob
 
-#Change filepath to wheresoever log files are stored.
-path = r'./'
-
-checked_files = Path(path).glob('*.txt')
+#Change filepath to where log files are stored.
+checked_files = glob.glob('./*.txt')
 li = list()
 for  f in checked_files:
     df = pd.read_csv(f, sep=", ", engine = "python")
-    df['DayOfLog'] = f.stem
     li.append(df)
 logframe = pd.concat(li, ignore_index=True)
-#Turning datafrom 
+#Forming table 
 duckdb.sql("CREATE TABLE logs AS SELECT * FROM logframe")
 
-#While complex in appearance, all this actually does is select the CustomerID from a subquery that contains every date interval from the uploaded logs with an interval of 1 day (meaning they were active on two consecutive days - the "start" and "end" date of the interval). striptime(Timestamp) was used consecutively to account for both VARCHAR and datetime datatypes from the logs since it is not specified.
-#Assumptions were made about the date format, but this can easily be altered to account for any particular date format. Knowing the date format would simplify this query allowing me to import it into a dateframe as the correct type, eliminating the need to use strptime to convert.                 
+
+
+# While complex in appearance, all this actually does is select the CustomerID from a subquery that contains every date interval from the uploaded 
+# logs with an interval of 1 day (meaning they were active on two consecutive days - the "start" and "end" date of the interval). 
+
+# striptime(Timestamp) was used consecutively to account for both VARCHAR and datetime datatypes from the logs since it is not specified.
+
+# Assumptions were made about the date format, but this can easily be altered to account for any particular date format. 
+# Knowing the date format would simplify this query allowing me to import it into a dateframe as the correct type, eliminating the need to use strptime to convert.                 
+
+
+
 result = duckdb.sql("SELECT DISTINCT CustomerID as LoyalCustomer FROM \
                     (SELECT DISTINCT CustomerID, PageID, strptime(timestamp[:-12], '%Y-%m-%d') - LAG(strptime(timestamp[:-12], '%Y-%m-%d')) \
                     OVER (PARTITION BY CustomerID ORDER BY strptime(timestamp[:-12], '%Y-%m-%d')) AS DIFFERENCE \
--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-jkj
+# DELMAR Technical Assessment Submission
+
+Programming language used: Python, SQL
+
+Outstanding questions for client:
+* How frequently would log files be queried to determine loyal customer status?
+    * How many files would be queried at a time?
+* Are there any performance constraints?
+* How are the log files structured?
+* How are the log files stored/retrieved.
+* What quantity of data is being analyzed?
+* Are there any preferred technologies or methods of implementation?
+
+
+
+
+
+Notes: 
+* Assuming comma delimited data as well as YYYY-MM-DD 
+* Goal is to write python program to read log files into some type of datastructure.
+    * Python for its versatility in data analysis, which seems to be the functional application of this question.
+* Using glob and pandas libraries for ease of mass-appendage and dataframe conversion.
+    * Landed on dataframes as I inted to use SQL to query the data due to my familiarity with it and lack of detailed requirements.
+    * Could be achieved through chaining pandas functions(?)
+* Program was inevitably designed to accept only two files as input, requiring user to specify the two days. 
+    * Not very scalable or versatile, but an extremely simple, efficient solution depending on business requirements.
+* Altered program to use subqueries at the cost of efficiency to query *multiple* files checking for users with consecutive sign on dates.
+    * This operates under the assumption that the date *is* included in the timestamp. 
+        * Only accomodates YYYY-MM-DD date format for the sake of this assessment, but in a business scenario, I'd have confirmed the format before writing the query. 
